dc5b34afa48fb9e42b72f8d41db7ee5102c65641
smorzhov <smorzhov@gmail.com>
2018-02-16 19:59:49 +0300

    Augmented data can be also cleaned with clean_text.py script

--- src/clean_text.py
+++ src/clean_text.py
@@ -7,6 +7,7 @@ Also, it can be imported into another module
 """
 import argparse
 import HTMLParser
+from string import punctuation
 from itertools import groupby
 from os import path
 from multiprocessing import Process, Pool, cpu_count
@@ -14,10 +15,12 @@ import pandas as pd
 import re
 import numpy as np
 from nltk.corpus import stopwords
-from utils import PROCESSED_DATA_PATH, RAW_DATA_PATH, try_makedirs
+from utils import PROCESSED_DATA_PATH, RAW_DATA_PATH, AUGMENTED_TRAIN_FILES, \
+                  try_makedirs, get_stop_words
 
 HTML_PARSER = HTMLParser.HTMLParser()
-STOP_WORDS = set(stopwords.words('english'))
+# STOP_WORDS = set(stopwords.words('english'))
+STOP_WORDS = get_stop_words()
 """
 the web url matching regex used by markdown
 http://daringfireball.net/2010/07/improved_regex_for_matching_urls
@@ -25,8 +28,15 @@ https://gist.github.com/gruber/8891611
 """
 URL_REGEX = r"""(?i)\b((?:https?:(?:/{1,3}|[a-z0-9%])|[a-z0-9.\-]+[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)/)(?:[^\s()<>{}\[\]]+|\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\))+(?:\([^\s()]*?\([^\s()]+\)[^\s()]*?\)|\([^\s]+?\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’])|(?:(?<!@)[a-z0-9]+(?:[.\-][a-z0-9]+)*[.](?:com|net|org|edu|gov|mil|aero|asia|biz|cat|coop|info|int|jobs|mobi|museum|name|post|pro|tel|travel|xxx|ac|ad|ae|af|ag|ai|al|am|an|ao|aq|ar|as|at|au|aw|ax|az|ba|bb|bd|be|bf|bg|bh|bi|bj|bm|bn|bo|br|bs|bt|bv|bw|by|bz|ca|cc|cd|cf|cg|ch|ci|ck|cl|cm|cn|co|cr|cs|cu|cv|cx|cy|cz|dd|de|dj|dk|dm|do|dz|ec|ee|eg|eh|er|es|et|eu|fi|fj|fk|fm|fo|fr|ga|gb|gd|ge|gf|gg|gh|gi|gl|gm|gn|gp|gq|gr|gs|gt|gu|gw|gy|hk|hm|hn|hr|ht|hu|id|ie|il|im|in|io|iq|ir|is|it|je|jm|jo|jp|ke|kg|kh|ki|km|kn|kp|kr|kw|ky|kz|la|lb|lc|li|lk|lr|ls|lt|lu|lv|ly|ma|mc|md|me|mg|mh|mk|ml|mm|mn|mo|mp|mq|mr|ms|mt|mu|mv|mw|mx|my|mz|na|nc|ne|nf|ng|ni|nl|no|np|nr|nu|nz|om|pa|pe|pf|pg|ph|pk|pl|pm|pn|pr|ps|pt|pw|py|qa|re|ro|rs|ru|rw|sa|sb|sc|sd|se|sg|sh|si|sj|Ja|sk|sl|sm|sn|so|sr|ss|st|su|sv|sx|sy|sz|tc|td|tf|tg|th|tj|tk|tl|tm|tn|to|tp|tr|tt|tv|tw|tz|ua|ug|uk|us|uy|uz|va|vc|ve|vg|vi|vn|vu|wf|ws|ye|yt|yu|za|zm|zw)\b/?(?!@)))"""
 EMOJI_PATTERN = re.compile(
-    u"(?<!&)#(\w|(?:[\xA9\xAE\u203C\u2049\u2122\u2139\u2194-\u2199\u21A9\u21AA\u231A\u231B\u2328\u2388\u23CF\u23E9-\u23F3\u23F8-\u23FA\u24C2\u25AA\u25AB\u25B6\u25C0\u25FB-\u25FE\u2600-\u2604\u260E\u2611\u2614\u2615\u2618\u261D\u2620\u2622\u2623\u2626\u262A\u262E\u262F\u2638-\u263A\u2648-\u2653\u2660\u2663\u2665\u2666\u2668\u267B\u267F\u2692-\u2694\u2696\u2697\u2699\u269B\u269C\u26A0\u26A1\u26AA\u26AB\u26B0\u26B1\u26BD\u26BE\u26C4\u26C5\u26C8\u26CE\u26CF\u26D1\u26D3\u26D4\u26E9\u26EA\u26F0-\u26F5\u26F7-\u26FA\u26FD\u2702\u2705\u2708-\u270D\u270F\u2712\u2714\u2716\u271D\u2721\u2728\u2733\u2734\u2744\u2747\u274C\u274E\u2753-\u2755\u2757\u2763\u2764\u2795-\u2797\u27A1\u27B0\u27BF\u2934\u2935\u2B05-\u2B07\u2B1B\u2B1C\u2B50\u2B55\u3030\u303D\u3297\u3299]|\uD83C[\uDC04\uDCCF\uDD70\uDD71\uDD7E\uDD7F\uDD8E\uDD91-\uDD9A\uDE01\uDE02\uDE1A\uDE2F\uDE32-\uDE3A\uDE50\uDE51\uDF00-\uDF21\uDF24-\uDF93\uDF96\uDF97\uDF99-\uDF9B\uDF9E-\uDFF0\uDFF3-\uDFF5\uDFF7-\uDFFF]|\uD83D[\uDC00-\uDCFD\uDCFF-\uDD3D\uDD49-\uDD4E\uDD50-\uDD67\uDD6F\uDD70\uDD73-\uDD79\uDD87\uDD8A-\uDD8D\uDD90\uDD95\uDD96\uDDA5\uDDA8\uDDB1\uDDB2\uDDBC\uDDC2-\uDDC4\uDDD1-\uDDD3\uDDDC-\uDDDE\uDDE1\uDDE3\uDDEF\uDDF3\uDDFA-\uDE4F\uDE80-\uDEC5\uDECB-\uDED0\uDEE0-\uDEE5\uDEE9\uDEEB\uDEEC\uDEF0\uDEF3]|\uD83E[\uDD10-\uDD18\uDD80-\uDD84\uDDC0]|(?:0\u20E3|1\u20E3|2\u20E3|3\u20E3|4\u20E3|5\u20E3|6\u20E3|7\u20E3|8\u20E3|9\u20E3|#\u20E3|\\*\u20E3|\uD83C(?:\uDDE6\uD83C(?:\uDDEB|\uDDFD|\uDDF1|\uDDF8|\uDDE9|\uDDF4|\uDDEE|\uDDF6|\uDDEC|\uDDF7|\uDDF2|\uDDFC|\uDDE8|\uDDFA|\uDDF9|\uDDFF|\uDDEA)|\uDDE7\uD83C(?:\uDDF8|\uDDED|\uDDE9|\uDDE7|\uDDFE|\uDDEA|\uDDFF|\uDDEF|\uDDF2|\uDDF9|\uDDF4|\uDDE6|\uDDFC|\uDDFB|\uDDF7|\uDDF3|\uDDEC|\uDDEB|\uDDEE|\uDDF6|\uDDF1)|\uDDE8\uD83C(?:\uDDF2|\uDDE6|\uDDFB|\uDDEB|\uDDF1|\uDDF3|\uDDFD|\uDDF5|\uDDE8|\uDDF4|\uDDEC|\uDDE9|\uDDF0|\uDDF7|\uDDEE|\uDDFA|\uDDFC|\uDDFE|\uDDFF|\uDDED)|\uDDE9\uD83C(?:\uDDFF|\uDDF0|\uDDEC|\uDDEF|\uDDF2|\uDDF4|\uDDEA)|\uDDEA\uD83C(?:\uDDE6|\uDDE8|\uDDEC|\uDDF7|\uDDEA|\uDDF9|\uDDFA|\uDDF8|\uDDED)|\uDDEB\uD83C(?:\uDDF0|\uDDF4|\uDDEF|\uDDEE|\uDDF7|\uDDF2)|\uDDEC\uD83C(?:\uDDF6|\uDDEB|\uDDE6|\uDDF2|\uDDEA|\uDDED|\uDDEE|\uDDF7|\uDDF1|\uDDE9|\uDDF5|\uDDFA|\uDDF9|\uDDEC|\uDDF3|\uDDFC|\uDDFE|\uDDF8|\uDDE7)|\uDDED\uD83C(?:\uDDF7|\uDDF9|\uDDF2|\uDDF3|\uDDF0|\uDDFA)|\uDDEE\uD83C(?:\uDDF4|\uDDE8|\uDDF8|\uDDF3|\uDDE9|\uDDF7|\uDDF6|\uDDEA|\uDDF2|\uDDF1|\uDDF9)|\uDDEF\uD83C(?:\uDDF2|\uDDF5|\uDDEA|\uDDF4)|\uDDF0\uD83C(?:\uDDED|\uDDFE|\uDDF2|\uDDFF|\uDDEA|\uDDEE|\uDDFC|\uDDEC|\uDDF5|\uDDF7|\uDDF3)|\uDDF1\uD83C(?:\uDDE6|\uDDFB|\uDDE7|\uDDF8|\uDDF7|\uDDFE|\uDDEE|\uDDF9|\uDDFA|\uDDF0|\uDDE8)|\uDDF2\uD83C(?:\uDDF4|\uDDF0|\uDDEC|\uDDFC|\uDDFE|\uDDFB|\uDDF1|\uDDF9|\uDDED|\uDDF6|\uDDF7|\uDDFA|\uDDFD|\uDDE9|\uDDE8|\uDDF3|\uDDEA|\uDDF8|\uDDE6|\uDDFF|\uDDF2|\uDDF5|\uDDEB)|\uDDF3\uD83C(?:\uDDE6|\uDDF7|\uDDF5|\uDDF1|\uDDE8|\uDDFF|\uDDEE|\uDDEA|\uDDEC|\uDDFA|\uDDEB|\uDDF4)|\uDDF4\uD83C\uDDF2|\uDDF5\uD83C(?:\uDDEB|\uDDF0|\uDDFC|\uDDF8|\uDDE6|\uDDEC|\uDDFE|\uDDEA|\uDDED|\uDDF3|\uDDF1|\uDDF9|\uDDF7|\uDDF2)|\uDDF6\uD83C\uDDE6|\uDDF7\uD83C(?:\uDDEA|\uDDF4|\uDDFA|\uDDFC|\uDDF8)|\uDDF8\uD83C(?:\uDDFB|\uDDF2|\uDDF9|\uDDE6|\uDDF3|\uDDE8|\uDDF1|\uDDEC|\uDDFD|\uDDF0|\uDDEE|\uDDE7|\uDDF4|\uDDF8|\uDDED|\uDDE9|\uDDF7|\uDDEF|\uDDFF|\uDDEA|\uDDFE)|\uDDF9\uD83C(?:\uDDE9|\uDDEB|\uDDFC|\uDDEF|\uDDFF|\uDDED|\uDDF1|\uDDEC|\uDDF0|\uDDF4|\uDDF9|\uDDE6|\uDDF3|\uDDF7|\uDDF2|\uDDE8|\uDDFB)|\uDDFA\uD83C(?:\uDDEC|\uDDE6|\uDDF8|\uDDFE|\uDDF2|\uDDFF)|\uDDFB\uD83C(?:\uDDEC|\uDDE8|\uDDEE|\uDDFA|\uDDE6|\uDDEA|\uDDF3)|\uDDFC\uD83C(?:\uDDF8|\uDDEB)|\uDDFD\uD83C\uDDF0|\uDDFE\uD83C(?:\uDDF9|\uDDEA)|\uDDFF\uD83C(?:\uDDE6|\uDDF2|\uDDFC))))[\ufe00-\ufe0f\u200d]?)+"
-)
+    u"(\ud83d[\ude00-\ude4f])|"  # emoticons
+    u"(\ud83c[\udf00-\uffff])|"  # symbols & pictographs (1 of 2)
+    u"(\ud83d[\u0000-\uddff])|"  # symbols & pictographs (2 of 2)
+    u"(\ud83d[\ude80-\udeff])|"  # transport & map symbols
+    u"(\ud83c[\udde0-\uddff])"  # flags (iOS)
+    "+",
+    flags=re.UNICODE)
+MARKS = {r'[\s]+.': '. ', r',': ', ', r'?': '? ', r'!': '! '}
+PUNCTUATION = set(punctuation)
 
 
 def init_argparse():
@@ -53,6 +63,10 @@ def init_argparse():
         default=PROCESSED_DATA_PATH,
         type=str)
     parser.add_argument(
+        '--load_augmented',
+        help='Use augmente data for training',
+        action='store_true')
+    parser.add_argument(
         '--cpus',
         nargs='?',
         help='Number of CPUs to use. By default, it will use all CPUs',
@@ -78,9 +92,17 @@ def remove_code_sequencies(comment, html=True, wiki_templates=False):
 
 def remove_punctuation(comment):
     """
-    It removes all punctuations except TODO
+    It removes all punctuations except !?,.'.
     """
-    return comment
+    # removes other punctuation
+    new_comment = re.sub(r"[^\w\s!?,.']", ' ', comment)
+    # removes duplicate punctuation
+    new_comment = re.sub(r"([\s!?,.'])\1+", r'\1', new_comment)
+    # remove spaces before punctuation
+    new_comment = re.sub(r"[\s]+(?=[!.,'])", '', new_comment)
+    # add space after punctuation
+    new_comment = re.sub(r"([!.,'])(?=[\w\d])", r'\1 ', new_comment)
+    return new_comment.strip()
 
 
 def remove_emojis(comment):
@@ -89,7 +111,7 @@ def remove_emojis(comment):
 
     Returns clean comment
     """
-    return EMOJI_PATTERN.sub('', comment)
+    return EMOJI_PATTERN.sub(' ', comment)
 
 
 def split_attached_words(comment):
@@ -112,7 +134,7 @@ def remove_urls(comment):
 
 def standardize_words(comment):
     """
-    looooove -> love
+    happpy -> happy :-), looooove -> loove :-(
 
     Returns clean comment
     """
@@ -121,7 +143,7 @@ def standardize_words(comment):
 
 def remove_stop_words(comment):
     """
-    This function removes all stop-words from comment
+    This function removes all stop-words and separate numbers from comment
 
     Args:
     - comment - raw comment string
@@ -130,7 +152,9 @@ def remove_stop_words(comment):
     """
     new_comment = []
     for word in comment.split():
-        if word.lower() not in STOP_WORDS:
+        # It filters out digits, punctuation and stop words
+        if (not word.replace('.', '', 1).isdigit() and
+                word not in PUNCTUATION and word.lower() not in STOP_WORDS):
             new_comment.append(word)
     return ' '.join(new_comment)
 
@@ -148,10 +172,12 @@ def clean_comment(comment):
     clean_comment = remove_code_sequencies(
         comment, html=True, wiki_templates=False)
     clean_comment = remove_urls(comment)
-    clean_comment = remove_stop_words(clean_comment)
     clean_comment = remove_emojis(clean_comment)
     clean_comment = standardize_words(clean_comment)
-    # clean_comment = remove_punctuation(clean_comment)
+    clean_comment = remove_punctuation(clean_comment)
+    clean_comment = remove_stop_words(clean_comment)
+    # let's remove punctuatiion once more
+    clean_comment = remove_punctuation(clean_comment)
     return clean_comment
 
 
@@ -182,24 +208,37 @@ def main():
 
     try_makedirs(args.processed)
 
-    train_proc = Process(
-        target=clean,
-        args=(
-            args.train,
-            path.join(args.processed, path.basename(args.train)),
-            args.cpus,
-        ))
-    test_proc = Process(
+    test_process = Process(
         target=clean,
         args=(
             args.test,
             path.join(args.processed, path.basename(args.test)),
-            args.cpus,
-        ))
-    train_proc.start()
-    test_proc.start()
-    train_proc.join()
-    test_proc.join()
+            args.cpus,))
+    train_processes = [
+        Process(
+            target=clean,
+            args=(
+                args.train,
+                path.join(args.processed, path.basename(args.train)),
+                args.cpus,))
+    ]
+    if args.load_augmented:
+        raw_prefix = path.splitext(args.train)[0]
+        processed_prefix = path.join(args.processed, path.basename(raw_prefix))
+        for suffix in AUGMENTED_TRAIN_FILES:
+            train_processes.append(
+                Process(
+                    target=clean,
+                    args=(
+                        raw_prefix + suffix,
+                        processed_prefix + suffix,
+                        args.cpus,)))
+    test_process.start()
+    for proc in train_processes:
+        proc.start()
+    test_process.join()
+    for proc in train_processes:
+        proc.join()
 
 
 if __name__ == '__main__':
